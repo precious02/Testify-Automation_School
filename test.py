60113bb63e13d35dfa993da8fd421094a25ce18b
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2023-10-11 13:59:41 +0100

    add unit test

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
+import main
+import unittest
 
-    import main
-    import unittest
-
-    class TestMain(unittest.TestCase):
+class TestMain(unittest.TestCase):
 
         def test_addition(self):
             self.assertEqual(main.addition(1, 2), 3, "Should be 3")
@@ -10,5 +9,5 @@
             self.assertEqual(main.addition(40, 20), 60, "Should be 60")
             self.assertEqual(main.addition(-3, 2), -1, "Should be -1")
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
         unittest.main()

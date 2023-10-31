3c8c45c1068daac6acaddb9ebbf914aa5d36a252
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2023-10-31 00:24:54 +0000

    JavaScript Tasks

---
+++ Module_3/Module_3A/Task13.js
@@ -0,0 +1,9 @@
+const books = {
+    title : 'Rich Dad Poor Dad',
+    description : 'Financial knowledge',
+    numberOfPages : 500,
+    author : 'Robert Kiyosaki',
+    reading : true
+}
+
+console.log(books['reading'])

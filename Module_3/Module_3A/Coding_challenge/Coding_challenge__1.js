5813ff51940d84b52be7e0e92db91619c3e0f8e6
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2023-11-09 00:59:08 +0000

    JavaScript Tasks

---
+++ Module_3/Module_3A/Coding_challenge/Coding_challenge__1.js
@@ -0,0 +1,11 @@
+//Calculate the sum of numbers within an array
+
+const NumArray = [2,4,6,8,10]
+let SumofNumbers = 0
+
+for (let i = 0; i<NumArray.length; i++)
+{
+    SumofNumbers= SumofNumbers+NumArray[i]
+}
+
+console.log(SumofNumbers)

ad22506056368dfdaeb61869aa311ac8f460f930
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2023-11-09 14:07:58 +0000

    JavaScript Tasks

---
+++ Module_3/Module_3A/Coding_challenge/Coding_challenge_7.js
@@ -0,0 +1,13 @@
+//Sort an array of numbers in descending order
+const scores = [4, 2, 10, 63, 3.5, 5]
+
+scores.sort(function (a, b) {
+    if (a > b) {
+        return -1;
+    }
+    if (b > a) {
+        return 1;
+    }
+    return 0;
+});
+console.log(scores)

af887a1c9e98e8b241d68dd5cf3c3318961de65b
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2023-12-06 22:16:21 +0000

    Java Tasks

---
+++ Module_4/Task/com/Testify/Task1.java
@@ -0,0 +1,10 @@
+package com.Testify;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am an Automation tester!");
+        System.out.println("Learning is fun!");
+        System.out.println("Java is exciting!");
+    }
+}

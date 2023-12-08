3c3c9eec2a3c0649296bb3af56db5352a9165e17
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2023-12-08 00:42:12 +0000

    Java Tasks

--- Module_4/Task/com/Testify/Task9a.java
+++ Module_4/Task/com/Testify/Task9a.java
@@ -1,2 +1,18 @@
-package com.Testify;public class Task9a {
+package com.Testify;
+
+public class Task9a {
+    public static void main(String[] args) {
+        for (int i=1; i<=10; i++){
+            if (i % 2 == 0){
+                System.out.println(i + " an even number");
+            }
+            else if (!(i % 2 == 0)){
+                System.out.println(i + " an odd number.");
+            }
+            else {
+                System.out.println("Null");
+            }
+
+        }
+    }
 }

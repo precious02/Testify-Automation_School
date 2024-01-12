d88c7491b65e0b6839d183671f6703e2f3996656
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-01-12 10:49:43 +0000

    Java Tasks

---
+++ src/test/java/org/example/Task5.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task5 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\hp\\Desktop\\Testify\\CI_CD_Clone\\ci_cdtestify\\Module4Bselenium\\src\\chromedriver.exe");
+        WebDriver driver5 = new ChromeDriver();
+        driver5.manage().window().maximize();
+        driver5.get("https://www.saucedemo.com/");
+
+        driver5.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/form/div[1]/input")).sendKeys("standard_user");
+        driver5.findElement(By.xpath("/html/body/div/div/div[2]/div[1]/div/div/form/div[2]/input")).sendKeys("secret_sauce");
+        driver5.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+        Thread.sleep(5000);
+
+        driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div/div/div/div[4]/div[2]/div[2]/button")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"shopping_cart_container\"]/a")).click();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//*[@id=\"checkout\"]")).click();
+        Thread.sleep(5000);
+
+    }
+}

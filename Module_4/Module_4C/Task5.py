c17b7608e70b13d29b2f78da7232131789bdba11
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-02-01 19:38:04 +0000

    Python tasks

--- Module_4/Module_4C/Task5.py
+++ Module_4/Module_4C/Task5.py
@@ -1,3 +1,8 @@
+# Declare a variable with value 2
+# Check if the variable is equal to 2, if true print out "Number is Two"
+# Add an else block which prints out "Number is not Two"
+
+
 myValue = 2
 if myValue == 2:
     print('Number is Two')
---
+++ Module_4/Module_4C/Task6.py
@@ -0,0 +1,23 @@
+# Declare two numbers with any values
+# Check if the first number is greater than 50 and check if the second number is less than 100,
+# if True print out "Evaluation is True"
+# Add an else block that prints "Evaluation is False"
+
+# Declare a string with value "python"
+# Check if the first string is equal to "testify" or "python",
+# if True prints out "Yay!! The string is a Testify Python
+
+firstVal = 5
+secVal = 10
+
+if firstVal > 50 and secVal < 100:
+    print('Evaluation is True')
+
+else:
+    print('Evaluation is False')
+
+
+favLang = 'python'
+
+if favLang == 'testify' or favLang == 'python':
+    print('Yay!! The string is a Testify Python')

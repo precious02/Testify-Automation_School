174036c7cc0cf2de0ac000d1fea9de2282433d90
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-02-01 18:10:47 +0000

    Python tasks

---
+++ Module_4/Module_4C/Task3.py
@@ -0,0 +1,14 @@
+# Declare a raw string
+
+aFact = r'I have a fine baby\nNo jokes'
+print(aFact)
+
+
+# Declare two strings with any value
+# Concatenate the two string
+# Print out the value of the concatenated strings
+
+janGoal = 'I want to learn '
+febGoal = 'swimming'
+myGoal = janGoal + febGoal
+print(myGoal)

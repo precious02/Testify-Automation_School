cbb6212abae0f3da235c10dc57d722a29e442239
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-02-01 17:54:03 +0000

    Python tasks

---
+++ Module_4/Module_4C/Task2.py
@@ -0,0 +1,12 @@
+# Declare a variable with variable_name as my_var and value as 12.6
+
+my_var = 12.6
+print(my_var)
+
+# Declare an integer variable with the value of 200
+preshnumber = 200
+print(preshnumber)
+
+# Declare a String variable with the value of your name
+myName = 'Precious'
+print(myName)

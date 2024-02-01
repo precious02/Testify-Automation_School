b4eac98efdc41c1b331790203a696962ef23e3f5
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-02-01 18:16:20 +0000

    Python tasks

---
+++ Module_4/Module_4C/Task4.py
@@ -0,0 +1,10 @@
+# Declare two numbers with any values
+# Add or Subtract or Multiply the two numbers together
+# Print out the value of the operation
+
+
+firstNum = 5
+secNum = 10
+result = firstNum + secNum
+
+print(result)

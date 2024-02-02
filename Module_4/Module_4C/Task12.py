fade164c20604634fb4284f0c90a28aa3ef66bd9
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-02-02 13:11:45 +0000

    Python tasks

---
+++ Module_4/Module_4C/Task12.py
@@ -0,0 +1,16 @@
+# Declare a global variable with name as language and the value as "Python"
+# Create a function, in the function declare a variable with name as language and value as "Java"
+# then print out the variable in the function
+# Print out the variable name into the console
+# Invoke the function
+
+language = "Python"
+
+
+def task():
+    language = "java"
+    print("Language", language)
+
+
+task()
+

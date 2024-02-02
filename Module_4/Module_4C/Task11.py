8e062215f159b9f6d9650af23cdf3048f5943c72
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-02-02 12:03:35 +0000

    Python tasks

---
+++ Module_4/Module_4C/Task11.py
@@ -0,0 +1,17 @@
+# Create a function that accepts two numbers, adds the numbers and prints out the result in the console.
+# Create a function that return the string value "Testify Python"
+# Invoke/call the two functions
+
+def add(num1=3, num2=5):
+    result = num1 + num2
+    print('Result: ', result)
+
+
+add()
+
+
+def preshstring():
+    return "Testify Python"
+
+
+print(preshstring())

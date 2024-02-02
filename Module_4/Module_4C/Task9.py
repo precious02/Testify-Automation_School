d3e62e2af0b34aa1c76bcbe8017ac89932965582
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-02-02 10:46:11 +0000

    Python tasks

---
+++ Module_4/Module_4C/Task9.py
@@ -0,0 +1,13 @@
+# Create a function that prints out "Hello World"
+# Create a function that does nothing, an empty function
+# Invoke/call both functions
+
+def greet():
+    print('Hello World')
+
+
+def presh():
+    pass
+
+greet()
+presh()

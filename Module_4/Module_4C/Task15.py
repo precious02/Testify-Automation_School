a411d81115d648013e652742726c7dbf1a19618d
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-02-06 19:38:46 +0000

    Python tasks

---
+++ Module_4/Module_4C/Task15.py
@@ -0,0 +1,15 @@
+# Declare a list with any value and number of item/element
+# Print the list in console
+# Sort the list
+# Print the list in console again
+
+cars = ['tesla', 'bmw', 'toyota', 'peugeot', 'astra']
+print('List of cars:', cars)
+
+# ascending order
+cars.sort()
+print('Sort-asc:', cars)
+
+# descending order
+cars.sort(reverse=True)
+print('Sort-desc:', cars)

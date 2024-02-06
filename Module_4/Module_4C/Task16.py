a01d7eb797cc629ea78f9fd33a28ed89dc77c9c0
PreciousNzekwe <precious.nzekwe@stu.cu.edu.ng>
2024-02-06 19:51:00 +0000

    Python tasks

---
+++ Module_4/Module_4C/Task16.py
@@ -0,0 +1,16 @@
+# Declare a dictionary with any key-value pair of items/elements
+# Print the dictionary in console
+# Update the dictionary with two different key-value pair items
+# Print the dictionary in console again
+
+stationery = {
+    'writing': 'pen',
+    'drawing': 'drawing board',
+    'ruling': 'ruler',
+    'shading': 'highlighter'
+}
+
+print('Dictionary:', stationery)
+
+stationery.update({'colouring': 'colour pencil', 'erasing': 'eraser'})
+print('Update:', stationery)
